e645156577b42127681053a0061eef8c221be546
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-26 23:51:31 +0000

    small

--- app/controllers/multiplication_controller.rb
+++ app/controllers/multiplication_controller.rb
@@ -10,6 +10,6 @@ class MultiplicationController < ApplicationController
 
     @result =  @first_number * @second_number
 
-    render({ :template => "multiplication_templates/multiply_results" })
+    render({ :template => "multiplication_templates/multiply_result" })
   end
 end
