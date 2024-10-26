9820701a1c62b3eaba6642a15a92f53fc7281335
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-26 23:58:26 +0000

    ok

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -4,7 +4,7 @@ class DivisionController < ApplicationController
   end
 
   def divide_these
-    @first_number = params.fetch("first_number")
+    @first_number = params.fetch("first_number").to_f
     @second_number = params.fetch("second_num").to_f
 
     @result =  @first_number / @second_number
